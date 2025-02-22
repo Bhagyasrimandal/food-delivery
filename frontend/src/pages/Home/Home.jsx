be46cfd029ceeb98af179cc3b748cca66083a777
Bhagyasrimandal <bhagyasrimandal689@gmail.com>
2025-02-22 12:10:53 +0530

    first commit

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Home.css'
+
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+
+const Home = () => {
+  const[category,setCategory]=useState("all");
+  return (
+    <div>
+        <Header/>
+        <ExploreMenu category={category} setCategory={setCategor}/>
+    </div>
+  )
+}
+
+export default Home
